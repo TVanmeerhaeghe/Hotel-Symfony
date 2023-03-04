576de5d7f4c968588a1b115cb50e7379bfd515ec
Teo <vanmeerhaegheteo@gmail.com>
2023-03-04 10:03:57 +0100

    Added Fixtures for Admin / Etablissement / Suite

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,13 +5,77 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+use Faker;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+use App\Entity\EtablissementHotel;
+use App\Entity\GerantHotel;
+use App\Entity\SuiteHotel;
+use App\Entity\Admin;
+
+
 class AppFixtures extends Fixture
 {
+    private $userPasswordHasherInterface;
+
+    public function __construct (UserPasswordHasherInterface $userPasswordHasherInterface)
+    {
+    $this->userPasswordHasherInterface = $userPasswordHasherInterface;
+    }
+
+    public function loadAdmin(ObjectManager $manager) {
+        $admin = new Admin();
+        $admin->setUserName('admin');
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $admin->setPassword(
+          $this->userPasswordHasherInterface->hashPassword(
+            $admin, 'admin'
+          )
+        );
+
+        $manager->persist($admin);
+        $manager->flush();
+    }
+
+    public function loadEtablissement(ObjectManager $manager, $faker) {
+        for($i=1; $i<=5;$i++) {
+            $etablissement = new EtablissementHotel();
+
+            $etablissement->setNom($faker->word());
+            $etablissement->setVille($faker->word());
+            $etablissement->setAdresse($faker->word());
+            $etablissement->setDescription($faker->paragraph());
+
+            $manager->persist($etablissement);
+            $this->addReference('etablissement_'.$i, $etablissement);
+        }
+        $manager->flush();
+    }
+
+    public function loadSuite(ObjectManager $manager, $faker) {
+
+        for($i=1; $i<=20;$i++) {
+            $suite = new SuiteHotel();
+            $currentEtablissement = $this->getReference('etablissement_'.mt_rand(1,5));
+            $suite->setEtablissementHotel($currentEtablissement);
+            $suite->setTitre($faker->word());
+            $suite->setImg($faker->image('public/uploads/img/', 640, 480, '', false));
+            $suite->setDescription($faker->paragraph());
+            $suite->setPrix(200);
+            $suite->setImgGallerie($faker->image('public/uploads/img/', 640, 480, '', false));
+            $manager->persist($suite);
+        }
+        $manager->flush();
+    }
+
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create("fr_FR");
 
-        $manager->flush();
+        $this->loadAdmin($manager);
+        $this->loadEtablissement($manager, $faker);
+        $this->loadSuite($manager,$faker);
     }
 }
